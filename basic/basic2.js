a2f2d49e604f16f6871fd9debf27d3fe06962189
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-05-21 14:24:36 +0530

    operator

---
+++ basic/basic2.js
@@ -0,0 +1,43 @@
+// var a = 10;
+// console.log("outer value of a = ",a);
+
+// let b = 20;
+// console.log("outer value of b = ",b);
+
+// {
+//     console.log("inner value of a = ",a);
+//     console.log("inner value of b = ",b);
+
+
+//     var c = 30;
+//     console.log("inner value of c = ",c);
+
+//     let d = 40;
+//     console.log("inner value of d = ",d);
+    
+// }
+
+// console.log("outer value of c = ",c);
+// console.log("outer value of d = ",d);
+
+// var a = 10;
+// console.log("outer value of a = ",a);
+
+
+// var a = 20;
+// console.log("outer value of a = ",a);
+
+
+// let a = 10;
+// console.log("outer value of a = ",a);
+
+
+// a = 20;
+// console.log("outer value of a = ",a);
+
+
+const pi = 3.14;
+console.log("pi = ",pi);
+
+pi = 3.14;
+console.log("pi = ",pi);
