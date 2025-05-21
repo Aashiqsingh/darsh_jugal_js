98e8dc6e55dc7901becd347f40051c0d0b0356f2
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-05-21 14:43:55 +0530

    ifElse

---
+++ ifElse/ifElse.js
@@ -0,0 +1,67 @@
+// var age = 16;
+
+// if(age > 18)
+// {
+//     console.log("adult");
+// }
+// else{
+//     console.log("child");
+// }
+
+// var a = 30;
+// var b = 20;
+
+// if(a > b){
+//     console.log("a is greater than b");
+// }
+// else{
+//     console.log("b is greater than a");
+// }
+
+
+var a = 50;
+var b = 60;
+var c = 70;
+
+
+if(a > b)
+{
+    if(a > c){
+        console.log("A is gretter");
+        
+    }
+    else{
+        console.log("C is greater");
+    }
+}
+else{
+    if(b > c)
+    {
+        console.log("B is greater");
+    }
+    else{
+        console.log("C is greater");
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+// if(a > b && a > c)
+// {
+//     console.log("A is grettr");
+    
+// }
+// else if(b > a && b > c)
+// {
+//     console.log("B is greater");
+// }
+// else{
+//     console.log("C is greater");
+// }
---
+++ operator/operator.js
@@ -0,0 +1,16 @@
+// console.log(3**4);
+
+var a = "10";
+var b = "10";
+
+
+// console.log(a == b);
+// console.log(a != b);
+// console.log(a === b);
+// console.log(a !== b);
+
+sum = sum + i 
+
+sum += i 
+
+
