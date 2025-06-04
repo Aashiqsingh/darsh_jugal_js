e2848fac0096f297c8cc2a35dcd984f18c8624ec
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-06-04 14:57:03 +0530

    string

---
+++ jugal/loop.js
@@ -0,0 +1,76 @@
+// for(let i=0;i<=5;i++)
+// {
+//     // console.log("hello" , i);
+//     console.log(i);
+    
+// }
+
+
+// let i = 1;
+
+// while(i <= 5)
+// {
+//     console.log(i);
+//     i++;
+// }
+
+
+// let i = 0;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i <= 5)
+
+
+// let num = 19;
+// let flag = true;
+// for(let i=2;i<num;i++)
+// {
+//     if(num % i == 0)
+//     {
+//         flag = false;
+//     }
+// }
+// if(flag == true)
+// {
+//     console.log("Number is Prime");
+// }
+// else{
+//     console.log("Number is not Prime");
+// }
+
+
+
+// let num = 18;
+
+// for(let i=1;i<=10;i++)
+// {
+//     console.log(num + " * " + i + " = " + num*i);
+    
+// }
+
+
+// let num = 3456;
+// var rev = 0;
+// while(num !=0)
+// {
+//     let rem = num % 10;
+//     rev = rev * 10 + rem;
+//     num = Math.floor(num / 10);
+// }
+// console.log(rev);
+
+
+// console.log(5/2);
+// console.log(Math.floor(9.99));
+// console.log(Math.ceil(9.09));
+
+// console.log(Math.round(9.49));
+
+// console.log(Math.max(5,10,15,20,44,55,23,12,4));
+// console.log(Math.min(5,10,15,20,44,55,23,12,4));
+
+console.log(Math.floor(1000 + Math.random() * 9000));
+
+
+
---
+++ string/script.js
@@ -0,0 +1,75 @@
+// var str2 = " Dhiraj sir"
+
+
+// console.log(str);
+// console.log(str.length);
+
+// let x = str.charAt(1)
+// console.log(x);
+
+// let x = str.charCodeAt(0)
+// console.log(x);
+
+// let x = str.concat(str2)
+// console.log(x);
+
+// console.log(str + str2);
+
+// let x = str.endsWith("d.")
+// let x = str.startsWith("Roy")
+// let x = str.includes("soft")
+
+
+// console.log(x);
+
+
+// let x = str.indexOf("a")
+// let x = str.lastIndexOf("a")
+
+// console.log(x);
+
+// let x = str.repeat(3)
+// console.log(x);
+
+// var str = "Royal technosoft pvt ltd.";
+
+// let x = str.replace("soft","hard")
+// let x = str.replace("o","zzzz")
+// let x = str.replaceAll("o","z")
+
+// console.log(x);
+
+// let x = str.slice(1)
+// let x = str.slice(1,4)
+// let x = str.slice(-4,-1)
+
+// console.log(x);
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+// let x = str.split(" ")
+// let x = str.split("o")
+// let x = str.split("")
+
+// console.log(x);
+
+
+
+// var str = "     Dhiraj sir  ";
+// console.log(str.length);
+
+
+// // let x = str.trim()
+// // let x = str.trimStart()
+// let x = str.trimEnd()
+
+
+// console.log(x.length);
+
+
+
+// user : UserName with spacing : aashiqsingh
+
+
+// @aashiqsingh11
