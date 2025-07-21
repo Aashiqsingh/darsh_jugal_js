f11e5512d4e8198c9e9e732e6979eb8deb8ccaad
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-07-21 19:26:28 +0530

    array

---
+++ Array/array1.js
@@ -0,0 +1,53 @@
+
+// console.log(users);
+// console.log(users[0]);
+// console.log(users[1]);
+
+
+// for(let i=0;i<users.length;i++){
+//     console.log(users[i].toUpperCase());
+// }
+
+// console.log(users.length);
+// console.log(users);
+
+// // users.push("aashiq")
+// users.push("aashiq","vivek","rohan")
+
+// console.log(users);
+
+// users.pop()
+
+// console.log(users);
+
+// users.unshift("aashiq")
+// console.log(users);
+
+// users.shift()
+// console.log(users);
+
+
+var users = ["harsh","rajvi","joshi","prajapti","harshil","darsh"]
+
+
+// let x = users.includes("josh")
+// console.log(x);
+
+// let x = users.reverse()
+// console.log(x);
+
+// let x = users.slice(1,4)
+// console.log(x);
+
+// let x = users.sort()
+// console.log(x);
+
+// users.splice(3,0,"vivek")
+// users.splice(3,2,"vivek")
+
+// console.log(users);
+
+
+// darsh 
+
+// hsrad
