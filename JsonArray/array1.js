cbb629dbec0da0019350f54452991a700eb4fa45
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-07-28 19:24:58 +0530

    map , filter , reduce

---
+++ JsonArray/array1.js
@@ -0,0 +1,48 @@
+var students = [
+    {
+        id:1,
+        name:"rahul",
+        age:19,
+        email:"rahul@gmail.com",
+        address:"delhi"
+    },
+    {
+        id:2,
+        name:"harsh",
+        age:20,
+        email:"harsh@gmail.com",
+        address:"gujarat"
+    },
+    {
+        id:3,
+        name:"rohan",
+        age:21,
+        email:"rohan@gmail.com",
+        address:"punjab"
+    },
+    {
+        id:4,
+        name:"priya",
+        age:22,
+        email:"priya@gmail.com",
+        address:"mumbai"
+    }
+]
+
+// console.log(students);
+
+// for(let i=0;i<students.length;i++){
+//     console.log(students[i]);
+// }
+
+
+// let x = students.map((stu)=>{
+//     return stu.name.toUpperCase()
+// })
+// console.log(x);
+
+
+// let x = students.filter((stu)=>{
+//     return stu.age > 20
+// })
+// console.log(x);
